3b82af71f45a0e54346fd039bed2e3b573258238
LNPappas <LPappas@gmail.com>
2022-03-30 10:16:44 -0700

    apollo server

---
+++ APOLLO-GRAPHQL-SERVER/products/products.model.js
@@ -0,0 +1,59 @@
+const products = [
+  {
+    id: "redshoe",
+    description: "Red Shoe",
+    price: 42.12,
+    reviews: [],
+  },
+  {
+    id: "bluejean",
+    description: "Blue Jeans",
+    price: 55.55,
+    reviews: [],
+  },
+];
+
+function getAllProducts() {
+  return products;
+}
+
+function getProductsByPrice(min, max) {
+  return products.filter((product) => {
+    return product.price >= min && product.price <= max;
+  });
+}
+
+function getProductsById(id) {
+  return products.find((product) => {
+    return product.id === id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+
+  products.push(newProduct);
+  return newProduct;
+}
+
+function addNewProductReview(id, rating, comment) {
+  const newReview = { rating, comment };
+  const matchedProduct = getProductsById(id);
+  if (matchedProduct) {
+    matchedProduct.reviews.push(newReview);
+  }
+  return newReview;
+}
+
+module.exports = {
+  getAllProducts,
+  getProductsByPrice,
+  getProductsById,
+  addNewProduct,
+  addNewProductReview,
+};
