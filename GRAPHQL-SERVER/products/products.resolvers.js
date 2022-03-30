fbace334929f417341bbff4733831b491b77f5c0
LNPappas <LPappas@gmail.com>
2022-03-30 09:55:33 -0700

    mutations

--- GRAPHQL-SERVER/products/products.resolvers.js
+++ GRAPHQL-SERVER/products/products.resolvers.js
@@ -5,5 +5,23 @@ module.exports = {
     products: () => {
       return productsModel.getAllProducts();
     },
+    productsByPrice: (_, args) => {
+      return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+      return productsModel.getProductsById(args.id);
+    },
+  },
+  Mutation: {
+    addNewProduct: (_, args) => {
+      return productsModel.addNewProduct(args.id, args.description, args.price);
+    },
+    addNewProductReview: (_, args) => {
+      return productsModel.addNewProductReview(
+        args.id,
+        args.rating,
+        args.comment
+      );
+    },
   },
 };
